651c0da653f76850e84dbca27cdc3fd9b5b24452
Quinton Ashley <qashto@gmail.com>
2024-05-05 17:16:07 -0500

    1.0.0

---
+++ README.md
@@ -0,0 +1,22 @@
+# p5play-render
+
+Server side image rendering demo. Demonstrates how p5play can be used with the `jsdom` and `canvas` packages to make an image.
+
+## Requirements
+
+Install [node.js](https://nodejs.org). Then in a Terminal, open this folder and run `node install`, to install the necessary packages (dependencies) needed to run the server program.
+
+## Files Overview
+
+- `package.json` : contains info about the project and its dependencies.
+- `jsconfig.json` : a config file for VSCode that enables autocompletion for p5play.
+- `render.js` : renders art using Bun. (NOT WORKING)
+- `render-node.js` : renders art using Node.js.
+
+## Running the Server
+
+In a Terminal, open this folder and run `node render-node.js`. It forgoes the draw loop, rendering as fast as possible.
+
+The program should finish in under a second and you should see a new file, `test.png`, in the `out` folder.
+
+Why did I make this demo? Being able to render the server-side physics simulation could be helpful for debugging.
---
+++ render-node.js
@@ -0,0 +1,64 @@
+/**
+ * Run this command to start the program:
+ * node render.js
+ *
+ * Renders art server side as fast as possible.
+ *
+ * The npm packages `canvas` and `jsdom` must be installed
+ * to run this example.
+ */
+
+// imports the node file system package
+const fs = require('fs');
+
+// imports q5 and p5play
+require('q5');
+require('p5play');
+
+// to use q5.js global mode in node.js you'll need to assign
+// user implemented q5 functions, like draw, to the global object
+global.setup = setup;
+global.draw = draw;
+
+// the rest of your code can pretty much be written normally!
+// no preload function is needed with q5 global mode
+new Q5();
+
+new Canvas(800, 800);
+background('#444');
+
+let tri = new Sprite(0, 0);
+tri.image = loadImage('triangle.png');
+tri.direction = 45;
+tri.speed = 100;
+
+// disable the default 60fps draw loop
+noLoop();
+
+// optionally signify that q5 can run your setup function
+// once preloading is complete
+preload();
+
+function setup() {
+	// redraw should not be called inside setup (p5 doesn't allow it)
+	// so use setTimeout to call the start function after setup is done
+	setTimeout(start);
+}
+
+function start() {
+	// draw 12 frames as fast as possible! use redraw instead of draw
+	// to ensure that pre and post draw p5play hooks are called
+	redraw(12);
+
+	// save the canvas to a png image file
+	const buffer = canvas.toBuffer('image/png');
+	fs.writeFileSync(__dirname + '/out/test.png', buffer);
+
+	// log the time it took to create the image
+	const time = (performance.now() / 1000).toFixed(2);
+	log('Created test.png! In ' + time + ' seconds');
+}
+
+function draw() {
+	// empty for this example, which relies on p5play's automatic drawing
+}
